106549b9cab0cd03061813d6159b2122e864d2b0
vnxfsc <1047658287@qq.com>
2025-04-23 17:09:18 +0800

    feat: 添加代币创建事件监听功能

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use jito_protos::shredstream::{
     shredstream_proxy_client::ShredstreamProxyClient, SubscribeEntriesRequest,
 };
-use std::any::type_name;
 use chrono::Local;
-
-fn type_of<T>(_: &T) -> &'static str {
-    type_name::<T>()
-}
+use solana_sdk::pubkey::Pubkey;
+use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // 创建目标公钥(创建代币事件)
+    let target_pubkey = Pubkey::from_str("TSLvdd1pWpHVjahSpsvCXUbgwsL3JAcvokwaKt1eokM").unwrap();
+    
     let mut client = ShredstreamProxyClient::connect("http://45.77.55.124:9999")
         .await
         .unwrap();
@@ -20,9 +20,6 @@ async fn main() -> Result<(), std::io::Error> {
         .into_inner();
 
     while let Some(slot_entry) = stream.message().await.unwrap() {
-        println!("收到槽位条目类型: {}", type_of(&slot_entry));
-        println!("条目数据类型: {}", type_of(&slot_entry.entries));
-        
         let entries =
             match bincode::deserialize::<Vec<solana_entry::entry::Entry>>(&slot_entry.entries) {
                 Ok(e) => e,
@@ -32,46 +29,52 @@ async fn main() -> Result<(), std::io::Error> {
                 }
             };
 
-        println!(
-            "槽位 {}, 条目数: {}, 交易数: {}",
-            slot_entry.slot,
-            entries.len(),
-            entries.iter().map(|e| e.transactions.len()).sum::<usize>()
-        );
-
         // 解析每个交易
         for entry in entries {
-            println!("条目类型: {}", type_of(&entry));
-            println!("交易列表类型: {}", type_of(&entry.transactions));
-            
             for tx_data in entry.transactions {
-                println!("交易数据类型: {}", type_of(&tx_data));
                 let transaction = tx_data;
-                println!("[{}] 交易签名: {}", Local::now().format("%Y-%m-%d %H:%M:%S%.3f"), transaction.signatures[0]);
                 
                 match &transaction.message {
                     solana_sdk::message::VersionedMessage::V0(message) => {
-                        // 打印账户地址
-                        for account_key in &message.account_keys {
-                            println!("账户地址: {}", account_key);
-                        }
+                        // 检查账户地址列表中是否包含目标公钥(创建代币事件)
+                        if message.account_keys.contains(&target_pubkey) {
+                            println!("\n{}", "-".repeat(80));
+                            println!("[{}] Pumpfun内盘创建代币事件:", Local::now().format("%Y-%m-%d %H:%M:%S%.3f"));
+                            println!("交易签名: {}", transaction.signatures[0]);
+                            
+                            // 打印所有账户地址
+                            println!("账户地址列表:");
+                            for account_key in &message.account_keys {
+                                println!("  {}", account_key);
+                            }
 
-                        // 打印指令
-                        for instruction in &message.instructions {
-                            println!("程序ID: {}", message.account_keys[instruction.program_id_index as usize]);
-                            println!("指令数据: {:?}", instruction.data);
+                            // 打印指令
+                            println!("指令列表:");
+                            for instruction in &message.instructions {
+                                println!("  程序ID: {}", message.account_keys[instruction.program_id_index as usize]);
+                                println!("  指令数据: {:?}", instruction.data);
+                            }
                         }
                     }
                     solana_sdk::message::VersionedMessage::Legacy(message) => {
-                        // 打印账户地址
-                        for account_key in &message.account_keys {
-                            println!("账户地址: {}", account_key);
-                        }
+                        // 检查账户地址列表中是否包含Pumpfun内盘创建代币事件
+                        if message.account_keys.contains(&target_pubkey) {
+                            println!("\n{}", "-".repeat(80));
+                            println!("[{}] Pumpfun内盘创建代币事件:", Local::now().format("%Y-%m-%d %H:%M:%S%.3f"));
+                            println!("交易签名: {}", transaction.signatures[0]);
+                            
+                            // 打印所有账户地址
+                            println!("账户地址列表:");
+                            for account_key in &message.account_keys {
+                                println!("  {}", account_key);
+                            }
 
-                        // 打印指令
-                        for instruction in &message.instructions {
-                            println!("程序ID: {}", message.account_keys[instruction.program_id_index as usize]);
-                            println!("指令数据: {:?}", instruction.data);
+                            // 打印指令
+                            println!("指令列表:");
+                            for instruction in &message.instructions {
+                                println!("  程序ID: {}", message.account_keys[instruction.program_id_index as usize]);
+                                println!("  指令数据: {:?}", instruction.data);
+                            }
                         }
                     }
                 }
